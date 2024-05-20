309585b0a79fc68945044f1dc3d91285ab0df479
Muhamad Misykat Ali A <ahmad@sharingvision.com>
2024-05-20 09:55:09 +0700

    fix: adding timeout on api call

--- ingestion/team_stat.py
+++ ingestion/team_stat.py
@@ -1,17 +1,19 @@
 import requests
-import json
 from elasticsearch import Elasticsearch
+import os
+import dotenv
+
+dotenv.load_dotenv("../.env")
 
 url = "https://apiv3.apifootball.com/?action=get_teams&league_id=152&APIkey=b88b65156b6108c1af6febdbbd8e52ae8c7035b0f9609943a735a36a19d43391"
 
 # Connect to Elasticsearch with SSL certificate verification disabled
-client = Elasticsearch("https://localhost:9200", api_key=("FS_3j44BzwJTVWm0LVLl", "32Gr3iKaRUSky6xgi-td2w"), verify_certs=False)
+client = Elasticsearch("https://localhost:9200", api_key=(os.environ.get("ELASTIC_API_KEY")), verify_certs=False)
 
-response = requests.get(url)
+response = requests.get(url, timeout=120)
 
 if response.status_code == 200:
     data = response.json()
-    print("Received data:", data)
     documents = []
     for item in data:
         index_metadata = {
@@ -24,8 +26,7 @@ if response.status_code == 200:
         documents.append(item)
 
     try:
-        client.bulk(operations=documents, pipeline="ent-search-generic-ingestion")
-        print("Ingested data into Elasticsearch")
+        print(client.bulk(operations=documents, pipeline="ent-search-generic-ingestion"))
     except Exception as e:
         print("Failed to ingest data:", e)
 else:
